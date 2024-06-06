0b6a50b89f63e60ad22ee7a959a21d30ef54a57b
Christopher Hu <tellwhy92@hotmail.com>
2024-06-06 13:21:56 +0800

    update

--- project/instance_segmentation/coco8_seg/monitoring.py
+++ project/instance_segmentation/coco8_seg/monitoring.py
@@ -1,11 +1,29 @@
-import io
+import os
+import json
+import click
+import random
 import psycopg
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
 
+# create_table_statement = """
+#     DROP TABLE IF EXISTS cv_metrics;
+#     CREATE TABLE cv_metrics(
+#         timestamp timestamp,
+#         image_name VARCHAR(255),
+#         image_base64 TEXT,
+#         image_metadata JSONB
+#     )
+# """
 create_table_statement = """
-    DROP TABLE IF EXISTS cv_metrics;
-    CREATE TABLE cv_metrics(
+    CREATE TABLE IF NOT EXISTS cv_metrics (
         timestamp timestamp,
-        image_metadata JSON
+        image_name VARCHAR(255),
+        image_base64 TEXT,
+        image_metadata JSONB
     )
 """
 host = "localhost"
@@ -22,6 +40,36 @@ def prep_database():
         with psycopg.connect(f"host = {host} port = {port} dbname = {database} user = {user} password = {password}") as conn:
             conn.execute(create_table_statement)
 
-if __name__ == '__main__':
+def get_image_metadata(image_path):
+    with Image.open(image_path) as img:
+        width, height = img.size
+        img_format = img.format
+        img = img.convert('RGB')
+        img_data = np.array(img)
+
+        # convert the image to base64
+        buffered = BytesIO()
+        img.save(buffered, format = img_format)
+        img_base64 = base64.b64encode(buffered.getvalue()).decode()
+
+    ratio = width / height
+    area = width * height
+
+    return {"width": width, "height": height, "ratio": ratio, "area": area, "image_base64": img_base64}
+
+def insert_data(image_name, image_base64, image_metadata):
+    with psycopg.connect(f"host = {host} port = {port}  dbname = {database} user = {user} password = {password}", autocommit = True) as conn:
+        with conn.cursor() as curr:
+            curr.execute("INSERT INTO cv_metrics (timestamp, image_name, image_base64, image_metadata) VALUES (%s, %s, %s, %s)", (datetime.now(), image_name, image_base64, json.dumps(image_metadata)))
+
+@click.command()
+@click.option("--inference_datapath", default = "../../../dataset/coco8-seg/valid/images", help = "location where inference data is stored")
+def initialize(inference_datapath):
     prep_database()
-    print("Hello World!")
+    random_image = random.sample(os.listdir(inference_datapath), 1)
+    image_name = random_image[0]
+    image_metadata = get_image_metadata(image_path = os.path.join(inference_datapath, random_image[0]))
+    insert_data(image_name = image_name, image_base64 = image_metadata["image_base64"], image_metadata = {k: v for k, v in image_metadata.items() if k != 'image_base64'})
+
+if __name__ == '__main__':
+    initialize()
